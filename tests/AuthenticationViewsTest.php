2d8976341e0098755f788f087cc5c960a828bef5
lester <lester@townsquaredigital.com>
2016-05-07 16:14:28 -0400

    Adjust tests for password reset workflow

--- tests/AuthenticationViewsTest.php
+++ tests/AuthenticationViewsTest.php
@@ -15,18 +15,16 @@ class AuthenticationViewsTest extends TestCase
         'first_name' => 'Sara',
         'last_name' => 'Ryan',
         'email' => 'sara.ryan@townsquaremedia.com',
-        'password' => 'secret'
     ];
     private $new_user_info = [
         'first_name' => 'Chris',
         'last_name' => 'Ireland',
         'email' => 'chris.ireland@townsquaremedia.com',
-        'password' => 'secret'
     ];
 
     public function testLoginAsExistingUser()
     {
-        User::create($this->existing_user_info);
+        User::create($this->existing_user_info + ['password' => 'secret']);
         $this->visit('/')
             ->seePageIs('/login')
             ->see('Sign In')
@@ -38,37 +36,43 @@ class AuthenticationViewsTest extends TestCase
 
     public function testFailLoginAsExistingUser()
     {
-        User::create($this->new_user_info);
+        User::create($this->new_user_info + ['password' => 'secret']);
         $this->visit('/')
             ->seePageIs('/login')
             ->type('sara.ryan@townsquaremedia.com', 'email')
-            ->type('badpassword', 'password')
+            ->type('the_wrong_password', 'password')
             ->press('Sign In')
             ->seePageIs('/login');
     }
 
-    public function testPasswordResetWithoutToken()
+    public function testRequestPasswordResetLink()
     {
+        $this->artisan('db:seed');
+        $this->dontSeeInDatabase('password_resets', ['email' => 'sara.ryan@townsquaremedia.com']);
         $this->visit('/password/reset')
             ->seePageIs('/password/reset')
             ->see('Reset Password')
-            ->see('Email Me');
+            ->type('sara.ryan@townsquaremedia.com', 'email')
+            ->press('Email Me');
+        $this->seeInDatabase('password_resets', ['email' => 'sara.ryan@townsquaremedia.com']);
     }
 
-    public function testPasswordResetWithToken()
+    public function testFailRequestPasswordResetLink()
     {
-        User::create([
-            'first_name' => 'Chris',
-            'last_name' => 'Ireland',
-            'email' => 'chris.ireland@townsquaremedia.com',
-            'password' => 'secret'
-        ]);
+        $this->dontSeeInDatabase('password_resets', ['email' => 'john.doe@townsquaremedia.com']);
+        $this->visit('/password/reset')
+            ->seePageIs('/password/reset')
+            ->see('Reset Password')
+            ->type('john.doe@townsquaremedia.com', 'email')
+            ->press('Email Me');
+        $this->dontSeeInDatabase('password_resets', ['email' => 'john.doe@townsquaremedia.com']);
+    }
 
-        $this->seeInDatabase('users', [
-            'first_name' => 'Chris',
-            'last_name' => 'Ireland',
-            'email' => 'chris.ireland@townsquaremedia.com', 
-        ]);
+    public function testSeePasswordResetForm()
+    {
+        User::create($this->new_user_info + ['password' => 'secret']);
+
+        $this->seeInDatabase('users', $this->new_user_info);
 
         DB::table('password_resets')->insert([
             'email' => 'chris.ireland@townsquaremedia.com',
@@ -79,5 +83,14 @@ class AuthenticationViewsTest extends TestCase
         $this->visit('/password/reset/' . $token)
             ->see('Reset Password')
             ->dontSee('Email Me');
+    }
+
+    public function testFailSeePasswordResetForm()
+    {
+        $bad_token = str_random(32);
+        $this->dontSeeInDatabase('password_resets', ['token' => $bad_token]);
+        $response = $this->call('GET', '/password/reset/' + $bad_token);
+        $this->assertEquals(404, $response->status());
+
     }
 }
